574a12a52a8fe5096c485af777ecf20a558c9e6f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-23 12:52:57 -0300

    PorcentagemSalario

---
+++ src/PorcentagemSalario.java
@@ -0,0 +1,10 @@
+public class PorcentagemSalario {
+
+    public static void main(String[] args) {
+        double salario = 2450.30;
+        double porcentagem = 21;
+        double resultado = salario * (porcentagem / 100);
+
+        System.out.println(porcentagem + "% de " + "R$ " + salario + " é igual a: " + "R$ " + resultado);
+    }
+}
