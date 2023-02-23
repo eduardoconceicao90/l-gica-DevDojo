121615ae7005360cc242fe90e9b2c2a477b3170f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-23 10:02:39 -0300

    CalculadorDiferencaIdade

---
+++ src/CalculadorDiferencaIdade.java
@@ -0,0 +1,10 @@
+public class CalculadorDiferencaIdade {
+
+    public static void main(String[] args) {
+        int primeiraIdade = 30;
+        int segundaIdade = 45;
+        int diferencaIdade = primeiraIdade - segundaIdade;
+
+        System.out.println("A diferença entre as idades é: " + diferencaIdade);
+    }
+}
