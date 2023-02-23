86539a005e6e66ea4188cca2f662e68d6909ac3f
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-23 15:32:16 -0300

    Controle de fluxo IF-ELSE part 01

---
+++ src/controleDeFluxo/IfElse_part01.java
@@ -0,0 +1,14 @@
+package controleDeFluxo;
+
+public class IfElse_part01 {
+    public static void main(String[] args) {
+        boolean condicao = true;
+
+        if (condicao) {
+            System.out.println("Dentro do IF");
+        } else {
+            System.out.println("Dentro do Else");
+        }
+        System.out.println("Fora do IF");
+    }
+}
