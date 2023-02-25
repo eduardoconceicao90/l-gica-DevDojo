82e4f79e3060651f54cd2065f7bb5a3e4ec078f0
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-25 17:03:35 -0300

    Controle de fluxo IF-ELSE part 02

---
+++ src/controleDeFluxo/IfElse_part02.java
@@ -0,0 +1,13 @@
+package controleDeFluxo;
+
+public class IfElse_part02 {
+    public static void main(String[] args) {
+        int idade = 17;
+
+        if (idade >= 18) {
+            System.out.println("É Adulto!");
+        } else {
+            System.out.println("Não é adulto!");
+        }
+    }
+}
