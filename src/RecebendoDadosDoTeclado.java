506e437e0d8386a5a25efcd339c762b130bbe4a6
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-26 11:24:52 -0300

    RecebendoDadosDoTeclado

---
+++ src/RecebendoDadosDoTeclado.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RecebendoDadosDoTeclado {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scan.nextLine();
+
+        System.out.print("Digite sua idade: ");
+        int idade = scan.nextInt();
+
+        System.out.println("Nome digitado: " + nome);
+        System.out.println("Idade digitada: " + idade);
+
+        scan.close();
+    }
+}
