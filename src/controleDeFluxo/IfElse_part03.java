af1f4b0ea6278809dc9877731a48cd2789c79910
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-26 07:07:46 -0300

    Controle de fluxo IF-ELSE part 03

---
+++ src/controleDeFluxo/IfElse_part03.java
@@ -0,0 +1,19 @@
+package controleDeFluxo;
+
+public class IfElse_part03 {
+    public static void main(String[] args) {
+        double salario = 4700.50;
+        double porcentagem;
+        double resultado;
+
+        if (salario > 4500) {
+            porcentagem = 30;
+            resultado = salario * (porcentagem / 100);
+            System.out.println(porcentagem + "% = " + resultado);
+        } else {
+            porcentagem = 15;
+            resultado = salario * (porcentagem / 100);
+            System.out.println(porcentagem + "% = " + resultado);
+        }
+    }
+}
