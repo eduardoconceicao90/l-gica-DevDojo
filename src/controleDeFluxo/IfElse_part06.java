a88ee97555bf585bee95080dea7ba7eb1249c180
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-26 11:12:49 -0300

    Controle de fluxo IF-ELSE part 06

---
+++ src/controleDeFluxo/IfElse_part06.java
@@ -0,0 +1,19 @@
+package controleDeFluxo;
+
+public class IfElse_part06 {
+    public static void main(String[] args) {
+
+        String nome = "Eduardo";
+        int idade = 16;
+
+        if (idade <= 10) {
+            System.out.println("Nome: " + nome + ", Categoria Infantil");
+        } else if (idade > 10 && idade <= 15) {
+            System.out.println("Nome: " + nome + ", Categoria Juvenil");
+        } else if (idade >= 16 && idade <= 19) {
+            System.out.println("Nome: " + nome + ", Categoria Pré-adulto");
+        } else {
+            System.out.println("Nome: " + nome + ", Categoria Adulto");
+        }
+    }
+}
