d11444afd9e88e683bbb25ed9afbbe9c5407a78b
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-26 10:26:16 -0300

    Controle de fluxo IF-ELSE part 04

---
+++ src/controleDeFluxo/IfElse_part04.java
@@ -0,0 +1,14 @@
+package controleDeFluxo;
+
+public class IfElse_part04 {
+    public static void main(String[] args) {
+
+        int numero = 4;
+
+        if (numero % 2 == 0) {
+            System.out.println("Par");
+        } else {
+            System.out.println("Ímpar");
+        }
+    }
+}
