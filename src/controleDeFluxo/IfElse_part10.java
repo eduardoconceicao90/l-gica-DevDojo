267af2e0c4ee71d2edbccdde7c8ee99aec79db1a
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-05 15:30:37 -0300

    Controle de fluxo IF-ELSE part 10

---
+++ src/controleDeFluxo/IfElse_part10.java
@@ -0,0 +1,27 @@
+package controleDeFluxo;
+
+import java.util.Scanner;
+
+public class IfElse_part10 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("*** ALISTAMENTO ***");
+
+        System.out.print("Digite o sexo: ");
+        String sexo = scan.next();
+
+        System.out.print("Digite a idade: ");
+        Integer idade = scan.nextInt();
+
+        if (sexo.equals("M") && idade >= 18) {
+            System.out.println("Alistamento obrigatório!");
+        } else if (sexo.equals("F") && idade >= 18) {
+            System.out.println("Deseja se alistar?");
+        } else if ((sexo.equals("M") || sexo.equals("F")) && idade < 18) {
+            System.out.println("Alistamento não permitido!");
+        }
+
+        scan.close();
+    }
+}
