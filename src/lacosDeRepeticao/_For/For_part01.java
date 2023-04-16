1f84dfd06c98c34fd5a45c9b4bd24ff45ee14698
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-16 10:34:29 -0300

    Laço de repetição FOR part 01

---
+++ src/lacosDeRepeticao/_For/For_part01.java
@@ -0,0 +1,11 @@
+package lacosDeRepeticao._For;
+
+public class For_part01 {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println("Contador: " + i);
+        }
+    }
+}
