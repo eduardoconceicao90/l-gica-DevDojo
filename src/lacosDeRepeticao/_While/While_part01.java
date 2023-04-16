d2f347e038110309d38daa1a62b1c753b080e0f7
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-16 07:58:16 -0300

    Laço de repetição WHILE part 01

---
+++ src/lacosDeRepeticao/_While/While_part01.java
@@ -0,0 +1,14 @@
+package lacosDeRepeticao._While;
+
+public class While_part01 {
+
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        while (contador <= 10){
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+}
