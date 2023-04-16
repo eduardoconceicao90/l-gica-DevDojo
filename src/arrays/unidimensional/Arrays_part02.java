08d1bfcaa17af382a3c2795ea992f8816e184bba
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-04-16 16:42:15 -0300

    ArraysMultidimensionais part 01

--- src/arrays/unidimensional/Arrays_part02.java
+++ src/arrays/unidimensional/Arrays_part02.java
@@ -1,6 +1,6 @@
-package arrays;
+package arrays.unidimensional;
 
-public class Arrays_part03 {
+public class Arrays_part02 {
 
     public static void main(String[] args) {
         double nota1 = 7.5;
@@ -17,10 +17,10 @@ public class Arrays_part03 {
         notas[1] = 8.0;
         notas[2] = 3.0;
         notas[3] = 6.0;
-
-        for (int i = 0; i < notas.length; i++) {
-            System.out.println(notas[i]);
-        }
+        System.out.println(notas[0]);
+        System.out.println(notas[1]);
+        System.out.println(notas[2]);
+        System.out.println(notas[3]);
 
     }
 }
--- src/arrays/unidimensional/Arrays_part04.java
+++ src/arrays/unidimensional/Arrays_part04.java
@@ -1,8 +1,8 @@
-package arrays;
+package arrays.unidimensional;
 
 import java.util.Scanner;
 
-public class Arrays_part04_ExercicioMelhorado {
+public class Arrays_part04 {
 
     public static void main(String[] args) {
 
@@ -12,21 +12,22 @@ public class Arrays_part04_ExercicioMelhorado {
         int[] array2 = new int[3];
         int[] array3 = new int[3];
 
-        String resultadoMultiplicacaoArrays = "";
-
         for (int i = 0; i < array1.length; i++) {
-
             System.out.println("Vetor 1, posição " + i + ": ");
             array1[i] = scan.nextInt();
-
-            System.out.println("Vetor 2, posição " + i + ": ");
-            array2[i] = scan.nextInt();
-
-            array3[i] = array1[i] * array2[i];
-            resultadoMultiplicacaoArrays = resultadoMultiplicacaoArrays + array3[i] + " ";
         }
 
-        System.out.println(resultadoMultiplicacaoArrays);
+        for (int i = 0; i < array2.length; i++) {
+            System.out.println("Vetor 2, posição " + i + ": ");
+            array2[i] = scan.nextInt();
+        }
+
+        System.out.println("Vetor 3 multiplicado: ");
+        for (int i = 0; i < array3.length; i++) {
+            array3[i] = array1[i] * array2[i];
+            System.out.print(array3[i] + " ");
+        }
+
         scan.close();
     }
 }
